c8e40168e1e401bb8c597aed1d61f8c66e2f7771
Karkazou <karkazouabdellah@gmail.com>
2023-04-20 18:35:30 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
-int main()
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
-	    printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	        printf("Size of an int: %ld byte(s)\n", sizeof(int));
-		    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-		        printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-			    printf("Size of a float: %ld byte(s)\n", sizeof(float));
+        printf("Size of a char: %ld byte(s)\n", sizeof(char));
+        printf("Size of an int: %ld byte(s)\n", sizeof(int));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %ld byte(s)\n", sizeof(float));
+        return (0);
+}
+With these changes, your code should now comply with the Betty coding style and documentation standards, and you should not receive any errors or warnings when running the checker.
 
-			        return 0;
+
+
+
+
+
 }
