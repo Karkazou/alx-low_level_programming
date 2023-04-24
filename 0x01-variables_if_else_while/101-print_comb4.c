18a3f4cba69fecb6fbe3caca8592f764e12b7077
Karkazou <karkazouabdellah@gmail.com>
2023-04-24 14:32:05 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,14 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ *main - Entry 
+ *
+ * discription - write exactly the sentence 
+ *
+ * Return: always 0 (seccess)
  */
+
 int main(void)
 {
-	int n, m, l;
-
-	for (n = 48; n < 58; n++)
-	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        return (1);
 }
