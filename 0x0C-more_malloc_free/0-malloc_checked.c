eb6abbe31758c71768b4abaf0904e2d95cdf7c07
Karkazou <karkazouabdellah@gmail.com>
2023-05-25 15:11:20 -0700

    0x10

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * malloc_checked-allocates memory using malloc and exit if failed
+ *
+ * @b : int
+ *
+ * Return : pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = malloc(b);
+
+	if (m == 0);
+		exit(98);
+
+	return (m);
+
+}
